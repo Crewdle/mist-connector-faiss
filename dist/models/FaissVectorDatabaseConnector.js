545fdfc6e4aa3ded9710ed0771e6d134a7ac1d1c
Vincent Lamanna <vincent@crewdle.com>
2024-08-07 16:45:51 -0400

    Add content to vector database (#3)
    * Add content to vector database
    * Add max content size in search
    * Fix search when k is larger than db size
    * Add relevant documents

--- dist/models/FaissVectorDatabaseConnector.js
+++ dist/models/FaissVectorDatabaseConnector.js
@@ -9,47 +9,92 @@ class FaissVectorDatabaseConnector {
     /**
      * The constructor.
      */
-    constructor() { }
+    constructor() {
+        /**
+         * The documents in the database.
+         * @ignore
+         */
+        this.documents = [];
+        /**
+         * The document chunks in the database.
+         * @ignore
+         */
+        this.documentChunks = [];
+    }
+    /**
+     * Get the content of the database.
+     * @returns The content of the database.
+     */
+    getBuffer() {
+        var _a;
+        return ((_a = this.index) === null || _a === void 0 ? void 0 : _a.toBuffer()) || new ArrayBuffer(0);
+    }
     /**
      * Search for the k nearest vectors.
      * @param vector The vector to search for.
      * @param k The number of nearest vectors to return.
      * @param minRelevance The minimum relevance of the vectors.
-     * @param startingIndex The starting index of the vectors.
-     * @returns The IDs of the k nearest vectors.
+     * @param contentSize The size of the content to return (vector +/- contentSize, default 0).
+     * @returns The content of the k nearest vectors.
      */
-    search(vector, k, minRelevance, startingIndex) {
+    search(vector, k, minRelevance, contentSize = 0) {
         if (!this.index) {
             return [];
         }
-        if (startingIndex !== undefined && minRelevance !== undefined) {
-            const items = this.index.search(vector, 1000);
-            const newItems = items.labels.map((label, index) => ({ label, distance: items.distances[index] })).filter((item) => item.label >= startingIndex);
-            const filteredItems = newItems.filter((item) => item.distance >= minRelevance).slice(0, k).map((item) => item.label);
-            return filteredItems;
+        if (k > this.index.ntotal()) {
+            k = this.index.ntotal();
         }
-        return this.index.search(vector, k).labels;
+        const search = this.index.search(vector, k);
+        let ids = search.labels.map((label, index) => ({ label, distance: search.distances[index] }));
+        if (minRelevance !== undefined) {
+            ids = ids.filter((item) => item.distance >= minRelevance);
+        }
+        return ids.map((id) => {
+            const document = this.documents.find((doc) => doc.startIndex <= id.label && doc.startIndex + doc.length > id.label);
+            if (!document) {
+                throw new Error('Document not found');
+            }
+            const startIndex = Math.max(document.startIndex, id.label - contentSize);
+            const endIndex = Math.min(document.startIndex + document.length, id.label + contentSize + 1);
+            return {
+                content: this.documentChunks.slice(startIndex, endIndex).join(' '),
+                relevance: id.distance,
+                pathName: document.name,
+            };
+        });
     }
     /**
      * Insert vectors into the database.
-     * @param vectors The vectors to insert.
+     * @param name The name of the document.
+     * @param chunks The chunks of the document.
+     * @param vectors The vectors to the document to insert.
      */
-    insert(vectors) {
+    insert(name, chunks, vectors) {
         if (vectors.length === 0) {
             return;
         }
         this.createIndex(vectors[0].length);
         this.index.add(vectors.flat());
+        this.documents.push({ name, startIndex: this.documentChunks.length, length: chunks.length });
+        this.documentChunks.push(...chunks);
     }
     /**
      * Remove vectors from the database.
-     * @param ids The IDs of the vectors to remove.
+     * @param name The name of the document.
      */
-    remove(ids) {
+    remove(name) {
         if (!this.index) {
             return;
         }
-        this.index.removeIds(ids);
+        const document = this.documents.find((doc) => doc.name === name);
+        if (!document) {
+            return;
+        }
+        const startIndex = document.startIndex;
+        const endIndex = startIndex + document.length;
+        this.documentChunks = this.documentChunks.slice(startIndex, endIndex);
+        this.index.removeIds(Array.from({ length: document.length }, (_, index) => startIndex + index));
+        this.documents = this.documents.filter((doc) => doc.name !== name);
     }
     /**
      * Create the Faiss index if it does not exist.
